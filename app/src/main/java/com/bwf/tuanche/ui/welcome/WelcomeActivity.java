dc358a0edcd6a6f790992622b27c577b0e0b80b2
nothing928 <1569708272@qq.com>
2016-08-17 09:43:32 +0800

    welcome,guide

--- app/src/main/java/com/bwf/tuanche/ui/welcome/WelcomeActivity.java
+++ app/src/main/java/com/bwf/tuanche/ui/welcome/WelcomeActivity.java
@@ -1,12 +1,18 @@
 package com.bwf.tuanche.ui.welcome;
 
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 
 import com.bwf.framwork.base.BaseActivity;
+import com.bwf.framwork.share.SharePrefreceHelper;
+import com.bwf.framwork.utils.IntentUtils;
 import com.bwf.tuanche.R;
+import com.bwf.tuanche.ui.mainpager.MainPagerActivity;
 
-public class WelcomeActivity extends BaseActivity {
+public class WelcomeActivity extends BaseActivity implements Handler.Callback{
 
+    private Handler handler;
 
     @Override
     public int getContentViewId() {
@@ -25,11 +31,33 @@ public class WelcomeActivity extends BaseActivity {
 
     @Override
     public void initData() {
-
+        handler = new Handler(this);
+        handler.sendEmptyMessageDelayed(1,3000);
     }
 
     @Override
     public void onClick(View v) {
 
+    }
+
+    @Override
+    public boolean handleMessage(Message message) {
+        switch (message.what){
+            case 1:
+//                if (SharePrefreceHelper.getInstence(WelcomeActivity.this).isFirst())
+                    IntentUtils.openActivity(WelcomeActivity.this,GuideActivity.class);
+//                else
+//                    IntentUtils.openActivity(WelcomeActivity.this,MainPagerActivity.class);
+                finish();
+                break;
+        }
+
+        return false;
+    }
+
+    @Override
+    protected void onDestroy() {
+        handler.removeCallbacksAndMessages(null);
+        super.onDestroy();
     }
 }
